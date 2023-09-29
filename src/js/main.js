180d1880618af132b6b0779b3f09e4232ca4375d
gibonDavid <gibon.david@live.fr>
2023-09-29 15:30:33 +0200

    site plus ou moins fonctionnel

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 function validateForm() {
-  const form = document.forms['exo3form'];
+  const form = document.forms['form'];
 
   // nom des champs du formulaire
   const inputs = [
@@ -11,7 +11,7 @@ function validateForm() {
     'password',
     'email',
   ];
-
+  console.log(inputs);
   let valid = true;
 
   for (const input of inputs) {
@@ -21,56 +21,9 @@ function validateForm() {
     }
   }
 
-  const choiceBoxes = document.getElementById('choiceBox');
-
-  for (const choiceBox of choiceBoxs) {
-    if (choiceBox.value.trim() === '') {
-      alert(`${choiceBox} The field has not been filled in`);
-      valid = false;
-    }
-  }
-
-  const checkbox = document.getElementById('checkbox');
-
-  if (!checkbox.checked) {
-    alert('you have to check the box');
-    valid = false;
-  }
-
   if (valid === true) {
-    let valName = document.getElementById('name').value;
-    let valEmail = document.getElementById('email').value;
+    let valName = document.getElementById('lastName').value;
+
     console.log(valName);
-    console.log(valEmail);
-  }
-}
-
-const email3 = document.getElementById('email3').value;
-if (!email3 || !email3.includes('@')) {
-  alert('Veuillez saisir une adresse email valide.');
-  valid = false;
-}
-
-/* <label for='prenom'>Entrez votre prénom svp : </label>
-            <input type='text' name='prenom' id='prenom' maxlength='20' required>
-            <span id='missPrenom'></span><br></br> */
-
-const formatValid =
-  /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-
-formatValid.addEventListener('click', validation);
-
-function validation(event) {
-  //Si le champ est vide
-  if (prenom.validity.valueMissing) {
-    event.preventDefault();
-    missPrenom.textContent = 'Prénom manquant';
-    missPrenom.style.color = 'red';
-    //Si le format de données est incorrect
-  } else if (prenomValid.test(prenom.value) == false) {
-    event.preventDefault();
-    missPrenom.textContent = 'Format incorrect';
-    missPrenom.style.color = 'orange';
-  } else {
   }
 }
